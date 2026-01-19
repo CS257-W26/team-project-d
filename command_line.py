d5abad53a1fc1a5cd24d82f3fbc057aa2c19834b
Milly-S <songm4@carleton.edu>
2026-01-19 11:38:59 -0600

    created command line file

--- command_line.py
+++ command_line.py
@@ -2,3 +2,6 @@
 The eventual location for the command line interface (CLI) for the project.
 This will be the entry point for the project when run from the command line.
 '''
+
+def main():
+    pass
