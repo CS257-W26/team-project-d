8e3a406fed988a3fba75b6781b1512e37a1c4164
Milly-S <songm4@stearns.mathcs.carleton.edu>
2026-02-06 10:22:06 -0500

    added createtable.sql

--- Data/createtable.sql
+++ Data/createtable.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS deforestation_share_forest;
-CREATE TABLE deforestation_share_forest(
-  entity TEXT,
-  country_code TEXT,
-  year YEAR,
-  deforestation DOUBLE PRECISION,
+CREATE TABLE deforestation_share_forest (
+    entity TEXT,
+    country_code TEXT,
+    year INTEGER,
+    deforestation DOUBLE PRECISION
 );
 
 DROP TABLE IF EXISTS co2_emission_per_capita;
-CREATE TABLE co2_emission_per_capita(
+CREATE TABLE co2_emission_per_capita (
     entity TEXT,
-    year YEAR,
-    co2_per_capita DOUBLE PRECISION,
+    year INTEGER,
+    co2_per_capita DOUBLE PRECISION
 );
